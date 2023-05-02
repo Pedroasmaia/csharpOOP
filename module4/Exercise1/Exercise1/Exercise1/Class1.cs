e34f84e0aadec76eabb413f075a30aadb46cc528
Pedro Maia <pedroasmaia@outlook.com>
2023-05-02 17:34:47 -0300

    Exercise 1 | Module 4

---
+++ module4/Exercise1/Exercise1/Exercise1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class People
+    {
+        public string name;
+        public int age;
+    }
+}
---
+++ module4/Exercise1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise1;
+
+People First, Second;
+
+First = new People();
+Second = new People();
+
+Console.WriteLine("Escreva o 1° nome:");
+First.name = Console.ReadLine();
+Console.WriteLine($"Quantos anos tem {First.name} :");
+First.age = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva o 2° nome:");
+Second.name = Console.ReadLine();
+Console.WriteLine($"Quantos anos tem {Second.name} :");
+Second.age = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Second.age > First.age? $"A Pessoa mais velha é {Second.name}" : $"A Pessoa mais velha é {First.name}");
